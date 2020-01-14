19180617688e4f1957ca1652f889ef70028b177b
alceil <ashishshajisrampickal@gmail.com>
2020-01-14 15:57:46 +0530

    Add dice functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math'
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+Void changediceface()
+{
+  rightdiceno=random().nextint(6) + 1;
+  leftdiceno=random().nextint(6) + 1;
+}
+class Dicepage extends StatefulWidget {
+  @override
+  _DicepageState createState() => _DicepageState();
+}
+
+class _DicepageState extends State<Dicepage> {
+  int leftdiceno=1;
+  int rightdiceno=1;
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+    return Center(
+        child:Row(
+            children:<widget>[
+        expanded
+        (child:padding(
+            padding: const EdgeInsets.all(16.0)
+        child:flatbutton(
+          onpressed(){
+            setstate{
+
+            }
+
+    }
+    child:Image.asset(''),
+    )
+    ),
+
+    expanded
+    (child:padding(
+    padding: const EdgeInsets.all(16.0)
+    child:FlatButton(
+    onpressed(){
+    setstate{
+    changediceface()
+    }
+
+    }
+    child:Image.asset('images\dice$leftdiceno.png'),
+    ),
+
+    ]
+    );
+    }
 }
